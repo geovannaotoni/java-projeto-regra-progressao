b3aaada4cab243d9a9ccea063e4dee784ec3f097
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-20 09:17:02 -0300

    feat02: Inserir as notas obtidas

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -18,6 +18,7 @@ public class App {
     int quantidade = Integer.parseInt(quantidadeStr);
     String[] nomes = new String[quantidade];
     int[] pesos = new int[quantidade];
+    int[] notas = new int[quantidade];
 
     for (int i = 1; i <= quantidade; i += 1) {
       System.out.println("Digite o nome da atividade " + i + ":");
@@ -28,6 +29,13 @@ public class App {
       String pesoStr = scanner.nextLine();
       int peso = Integer.parseInt(pesoStr);
       pesos[i - 1] = peso;
+
+      System.out.println("Digite a nota obtida para " + nomes[i - 1] + ":");
+      String notaStr = scanner.nextLine();
+      int nota = Integer.parseInt(notaStr);
+      notas[i - 1] = nota;
     }
+
+    scanner.close();
   }
 }
